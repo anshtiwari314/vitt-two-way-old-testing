26aa2b455a6a814c24d9d6bb5504c7a6c9ba7a3e
anuj <anuj@localhost.localdomain>
2023-04-11 13:38:06 +0530

    port in peer js server changed to 6183

--- public/script-previous.js
+++ public/script-previous.js
@@ -107,7 +107,7 @@ let myStream
 //const peer = new Peer(myId,{'iceServers': [{ 'urls': 'stun:stun.l.google.com:19302' }]})
 const peer = new Peer(myId,{
     host: "vitt-peerjs-server-production.up.railway.app",
-    port: 443,
+    port: 6183,
     path: "/myapp",
 })
 
